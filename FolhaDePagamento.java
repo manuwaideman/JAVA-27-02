b8c4e77b658b2724c864e22ca377b507a7b232e8
Manoella Waideman <mherreriaswaideman@gmail.com>
2024-02-27 11:43:41 -0300

    Update FolhaDePagamento.java

--- FolhaDePagamento.java
+++ FolhaDePagamento.java
@@ -1,15 +1,16 @@
 package br.com.fiap;
 
-public class TesteFolhaDePagamento {
+public class FolhaDePagamento {
 	
-	public static void main(String[] args) {
-		FolhaDePagamento fp = new FolhaDePagamento();
-		fp.salarioBruto = 5000;
-		fp.numeroDeDependentes = 2;
-		fp.descontoINSS = 10;
-		fp.valorPlanoDeSaude = 50.00;
-		
-		 System.out.println("Salário liquido: " + fp.calcularSalarioLiquido());
+	public double salarioBruto;
+	public int numeroDeDependentes;
+	public double descontoINSS;
+	public double valorPlanoDeSaude;
+	
+	public double calcularSalarioLiquido() {
+		double inss, planoSaude;
+		inss = salarioBruto * (descontoINSS / 100);
+		planoSaude = valorPlanoDeSaude * (numeroDeDependentes + 1);
+		return salarioBruto - (inss + planoSaude);
 	}
-
 }
